0a88aa0b7cc722c3a51c1173d629a76528d4ddb2
ThabataG <thabata.helen@gmail.com>
2016-06-15 04:12:45 -0300

    Adicionando templates de views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publication
 
 def publication_list(request):
-    return render(request, 'blog/publication_list.html', {})
+	publication = Publication.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/publication_list.html', {'publication':publication})

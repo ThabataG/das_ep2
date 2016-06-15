23fce6a3f0ee5cd43d59d3a2be9b683f13624795
ThabataG <thabata.helen@gmail.com>
2016-06-15 14:47:32 -0300

    Adicionando uma publicação

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.publication_list),
+	url(r'^publication/(?P<pk>[0-9]+)/$', views.publication_detail),
+	url(r'^publication/new/$', views.publication_new, name='publication_new'),
+	url(r'^publication/(?P<pk>[0-9]+)/edit/$', views.publication_edit, name='publication_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,41 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Publication
+from .forms import PublicationForm
 
 def publication_list(request):
-	publication = Publication.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	publication = Publication.objects.order_by('published_date')
 	return render(request, 'blog/publication_list.html', {'publication':publication})
+
+def publication_detail (request, pk):
+	publication = get_object_or_404(Publication, pk=pk)
+	return render(request, 'blog/publication_detail.html', {'publication': publication})
+
+def publication_new(request):
+	if request.method == "POST":
+		form = PublicationForm(request.POST)
+		if form.is_valid():
+			publication = form.save (commit = False)
+			publication.author = request.user
+			publication.published_date = timezone.now()
+			publication.save()
+			return redirect ('blog.views.publication_detail', pk=publication.pk)
+	else:
+		form = PublicationForm()
+	return render (request, 'blog/publication_edit.html', {'form':form})
+
+def publication_edit(request, pk):
+	publication = get_object_or_404(Publication, pk=pk)
+	if request.method == "POST":
+		form = PublicationForm(request.POST, instance = publication)
+		if form.is_valid():
+			publication = form.save (commit = False)
+			publication.author = request.user
+			publication.published_date = timezone.now()
+			publication.save()
+			return redirect ('blog.views.publication_detail', pk=publication.pk)
+	else:
+		form = PublicationForm(instance = publication)
+	return render (request, 'blog/publication_edit.html', {'form': form})

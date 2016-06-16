d9b7e433d60b542c2da2414dda536808372c79c5
ThabataG <thabata.helen@gmail.com>
2016-06-15 22:18:16 -0300

    Refatorando blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Publication
 
-admin.site.register (Publication)
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ['author', 'title', 'published_date']
+	list_filter = ['author', 'title', 'published_date']
+	search_fields = ['title']
+	save_on_top = True
+	
+admin.site.register (Publication, BlogAdmin)
